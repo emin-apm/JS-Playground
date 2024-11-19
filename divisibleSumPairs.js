5db800eeb4324b06fd2bbecca442a9bfb7f781e2
Emin A <122976155+eminn-a@users.noreply.github.com>
2024-11-19 20:37:23 +0200

    divisibleSumPairs

---
+++ divisibleSumPairs.js
@@ -0,0 +1,17 @@
+function divisibleSumPairs(n, k, ar) {
+  let count = 0;
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      if ((ar[i] + ar[j]) % k === 0) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
+
+const n = 6;
+const k = 3;
+const ar = [1, 3, 2, 6, 1, 2];
+
+console.log(divisibleSumPairs(n, k, ar));
