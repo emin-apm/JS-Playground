8250d0a9f6b9314b9b20ab40aa6e544caad32cbd
Emin A <122976155+eminn-a@users.noreply.github.com>
2024-11-23 22:13:20 +0200

    billShare

---
+++ JS-Advanced/billShare.js
@@ -0,0 +1,17 @@
+function billShare(bill, k, b) {
+  let cost = 0;
+
+  for (let i = 0; i < bill.length; i++) {
+    if (k !== i) {
+      cost = cost + bill[i];
+    }
+  }
+  if (cost / 2 === b) {
+    console.log("Bon Appetit");
+  } else {
+    console.log(b - cost / 2);
+  }
+}
+
+// billShare([3, 10, 2, 9], 1, 12);
+billShare([3, 10, 2, 9], 1, 7);
